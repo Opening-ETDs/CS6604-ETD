b8be3e5768074059891be14321a0c3fbfb539d3a
Bipasha-banerjee <bips.ban@gmail.com>
2019-10-13 23:17:46 -0400

    added title surname and forename

--- Parsing/extractData.py
+++ Parsing/extractData.py
@@ -1,11 +1,12 @@
 import nltk
 import xml.etree.ElementTree as ET
 from lxml.etree import strip_tags, strip_elements, parse
-
+import os
+from os import listdir
 nltk.download('punkt')
 
 
-def extract_ch_json(doc_path, doc_type, doc_id):
+def extract_ch_json(doc_path,out_path, doc_type, doc_id):
     """Return JSON from Grobid TEI
     This function tries to group sections of Grobid-generated TEI as chapters.
     """
@@ -25,13 +26,36 @@ def extract_ch_json(doc_path, doc_type, doc_id):
     titles = doc.xpath('/html/body/tei:tei/tei:teiheader/tei:filedesc/tei:titlestmt/tei:title',
                        namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
 
-    divs = doc.xpath('/html/body/tei:tei/tei:text/tei:back/tei:div/tei:listbibl',
+    divs = doc.xpath('/html/body/tei:tei/tei:text/tei:back/tei:div/tei:listbibl/tei:biblstruct',
                      namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
     
-    
-    print(divs)
+   
+    for bib in divs: 
   
-
+        analytic = bib.findall('tei:analytic', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+        for x in analytic:
+            title = x.findall('tei:title',namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+            author = x.findall('tei:author/tei:persname',namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+            # for z in author:
+            #     fname = z.findall('tei:author',namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+            for mnmn in author:
+                forename = mnmn.findall('tei:forename',namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+                surname = mnmn.findall('tei:surname',namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+                for klabel in forename:
+                    file = open(out_path+"William_ref.txt","a+")
+                    file.write(klabel.text+", "+"forename"+"\n")
+                for klabel in surname: 
+                    file = open(out_path+"William_ref.txt","a+")
+                    file.write(klabel.text+", "+"surname"+"\n")
+            for y in title:  
+                
+                # if os.path.exists(out_path+"William_ref.txt"):
+                #     os.remove(out_path+"William_ref.txt")   
+                file = open(out_path+"William_ref.txt","a+")
+          
+                file.write(y.text+", "+"title"+"\n")
+                #print(y.text)
+                file.close
     return out_dict
 
 
@@ -42,9 +66,8 @@ if __name__ == '__main__':
     """
 
     docPath = '/Users/bipashabanerjee/Documents/CS/sem3/6604/grobid/dissertation/Williams_MR_D_2011.pdf.xml'
-    ch_json = extract_ch_json(docPath, 'thesis', 17292)
+    outPath = '/Users/bipashabanerjee/Documents/CS/sem3/6604/grobid/dissertation/'
+    ch_json = extract_ch_json(docPath,outPath, 'thesis', 17292)
 
-    for title in ch_json['title']:
-        print()
-        print(chapter['title'])
+  
         
