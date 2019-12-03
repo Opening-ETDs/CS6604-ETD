1cf890e68c82c7eebcc1579897912771563bdaf6
Bipasha-banerjee <bips.ban@gmail.com>
2019-12-02 23:24:25 -0500

    Update
    This file extracts fields from Grobid processed TEI/XML files.

--- Parsing/extractData.py
+++ Parsing/extractData.py
@@ -1,50 +1,83 @@
 import nltk
 import xml.etree.ElementTree as ET
 from lxml.etree import strip_tags, strip_elements, parse
-
+import os
+from os import listdir
 nltk.download('punkt')
 
-
-def extract_ch_json(doc_path, doc_type, doc_id):
+def extract_ch_json(doc_path,out_path, doc_type):
     """Return JSON from Grobid TEI
     This function tries to group sections of Grobid-generated TEI as chapters.
     """
-    if doc_type == 'dissertation':
-        return extract_ch_json_dissertation(doc_path, doc_type, doc_id)
+    if doc_type == 'thesis':
+        return extract_ch_json_thesis(doc_path, doc_type)
 
-    try:
-        doc = parse(doc_path)
-    except Exception as e:
-        print('%s: %s' % (type(e).__name__, e))
-        return 0
+    
 
-    # initial dict for information storage
-    out_dict = {'id': doc_id, 'type': doc_type, 'title': '', 'chapters': []}
-
-    # get document title
-    titles = doc.xpath('/html/body/tei:tei/tei:teiheader/tei:filedesc/tei:titlestmt/tei:title',
-                       namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
-
-    divs = doc.xpath('/html/body/tei:tei/tei:text/tei:back/tei:div/tei:listbibl',
+    docLabels = []
+    dir_exists = os.path.isdir(doc_path)
+    
+    if dir_exists:
+        docLabels = [f for f in listdir(doc_path) if not f.startswith('.') and not f.startswith('_') and  f.endswith('.xml')]
+        for id, f1 in enumerate(docLabels):
+            #print(f1)
+         
+            doc = parse(doc_path+f1)
+       
+            divs = doc.xpath("/tei:TEI/tei:text/tei:back/tei:div[@type='references']/tei:listBibl/tei:biblStruct",
                      namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
-    
-    
-    print(divs)
-  
+            filename = (f1.split('.')[0])
+            #print(filename)
+            i=0
 
-    return out_dict
+            for bib in divs:  
+                monogr = bib.findall('tei:monogr', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+                analytic = bib.findall('tei:analytic', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+                
+                for data in monogr:
+                    title = data.findall("tei:title[@type='main']",namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+                    for y in title:  
+                        i = i+1
+                        file = open(out_path+filename+"_title.txt","a+")
+                  
+                        file.write(y.text+", "+"title"+"\n")
+                        file.close()  
+                for author in monogr:
+                    author_data = author.findall('tei:author/tei:persName', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+                    for a in author_data:
+
+                        firstname=a.findall("tei:forename", namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+                        surname=a.findall("tei:surname",namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})
+                        for y2 in firstname:  
+                      
+                            file = open(out_path+filename+"_title.txt","a+")
+                            file.write(y2.text+", "+"first-name"+"\n")
+                            file.close() 
+                        for y3 in surname:
+                            file = open(out_path+filename+"_title.txt","a+")
+                            file.write(y3.text+", "+"surname"+"\n")
+                            file.close() 
+
+                for moreData in analytic:
+                    title_more = moreData.findall("tei:title[@type='main']",namespaces={'tei': 'http://www.tei-c.org/ns/1.0'}) 
+                    for y1 in title_more:  
+                        i = i+1
+                        file = open(out_path+filename+"_title.txt","a+")
+                      
+                        file.write(y1.text+", "+"title"+"\n")       
+                        file.close()
+            
+                
+
 
 
 if __name__ == '__main__':
+    print("file")
     """Extract sentences from Grobid TEI XML
     
     For each chapter, print the chapter title. Then print each sentence on a new line. 
     """
 
-    docPath = '/Users/bipashabanerjee/Documents/CS/sem3/6604/grobid/dissertation/Williams_MR_D_2011.pdf.xml'
-    ch_json = extract_ch_json(docPath, 'thesis', 17292)
-
-    for title in ch_json['title']:
-        print()
-        print(chapter['title'])
-        
+    doc_path = "/Users/bipashabanerjee/Documents/CS/sem3/6604/grobid/dataProcess/"
+    out_path = "/Users/bipashabanerjee/Documents/CS/sem3/6604/grobid/dataProcess/new_diss/"
+    ch_json = extract_ch_json(doc_path,out_path, 'dissertation')
